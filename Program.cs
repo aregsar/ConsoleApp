0d54fe07f7546284a2b2c8f53fe5b2359b41c672
Areg Sarkissian <aregsar@gmail.com>
2020-12-14 12:11:01 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Print("Hello World!");
+
+foreach (var arg in args)
+    Print(arg);
+
+Console.WriteLine("Type text to print or hit enter to exit");
+
+while (true)
+{
+    var lineText = Console.ReadLine();
+
+    if (lineText.Length == 0)
+        break;
+
+    Print(lineText);
+}
+
+void Print(string outputText) =>
+    Console.WriteLine(outputText);
